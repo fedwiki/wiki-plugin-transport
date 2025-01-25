dd86865955be33452a8606e1b6d710a6511f923d
Paul Rodwell <paul.rodwell@btinternet.com>
2025-01-25 11:51:49 +0000

    convert to javascript

--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
-# build time tests for transport plugin
-# see http://mochajs.org/
+// build time tests for transport plugin
+// see http://mochajs.org/
 
-transport = require '../client/transport'
-expect = require 'expect.js'
+import { transport } from '../src/client/transport.js'
+import { describe, it } from 'node:test'
+import expect from 'expect.js'
 
-describe 'transport plugin', ->
-
-  describe 'expand', ->
-
-    it 'can make itallic', ->
-      result = transport.expand 'hello *world*'
-      expect(result).to.be 'hello <i>world</i>'
+describe('transport plugin', () => {
+  describe('expand', () => {
+    it('can make itallic', () => {
+      const result = transport.expand('hello *world*')
+      expect(result).to.be('hello <i>world</i>')
+    })
+  })
+})
